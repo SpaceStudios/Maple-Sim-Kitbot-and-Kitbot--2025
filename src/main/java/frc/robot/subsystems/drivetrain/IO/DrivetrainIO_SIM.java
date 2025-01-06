2792482a07e5433328c747c21b0799674af99d73
macsk <mac.skaanes@gmail.com>
2025-01-05 19:52:01 -0500

    Add more things

--- src/main/java/frc/robot/subsystems/drivetrain/IO/DrivetrainIO_SIM.java
+++ src/main/java/frc/robot/subsystems/drivetrain/IO/DrivetrainIO_SIM.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 
 import org.ironmaple.simulation.drivesims.SwerveDriveSimulation;
 import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
-import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -18,19 +17,22 @@ import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
 
-import com.fasterxml.jackson.databind.node.POJONode;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.estimator.PoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DesignConstants;
@@ -45,9 +47,13 @@ public class DrivetrainIO_SIM implements DrivetrainIO {
     DriveTrainSimulationConfig driveSimConfig;
     SwerveDriveSimulation driveSim;
     VisionSystemSim visionSim;
-    PhotonPoseEstimator poseEstimator;
+    PhotonPoseEstimator poseEstimatorCam1;
+    PhotonPoseEstimator poseEstimatorCam2;
+    DifferentialDrivePoseEstimator sim_PoseEstimator;
     Pose2d previousPose;
     PhotonCameraSim cam1Sim;
+    PhotonCameraSim cam2Sim;
+    DifferentialDriveOdometry driveOdometry;
 
     public DrivetrainIO_SIM() {
         leftPid = new PIDController(0.9, 0, 0);
@@ -78,12 +84,21 @@ public class DrivetrainIO_SIM implements DrivetrainIO {
         cameraProperties.setLatencyStdDevMs(5);
         PhotonCamera cam1 = new PhotonCamera("camera1");
         cam1Sim = new PhotonCameraSim(cam1, cameraProperties);
-        Translation3d cam1Pos = new Translation3d(0.1, 0, 0);
-        Rotation3d cam1Rot = new Rotation3d(0, Units.degreesToRadians(-15), 0);
+        Translation3d cam1Pos = new Translation3d(0.1, 0, 0.1);
+        Rotation3d cam1Rot = new Rotation3d(0, Units.degreesToRadians(-15), Units.degreesToRadians(25));
         Transform3d cam1Transform = new Transform3d(cam1Pos, cam1Rot);
         visionSim.addCamera(cam1Sim, cam1Transform);
-        poseEstimator = new PhotonPoseEstimator(fieldTags, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam1Transform);
+        PhotonCamera cam2 = new PhotonCamera("camera2");
+        cam2Sim = new PhotonCameraSim(cam2, cameraProperties);
+        Translation3d cam2Pos = new Translation3d(0.1, 0, -0.1);
+        Rotation3d cam2Rot = new Rotation3d(0, Units.degreesToRadians(-15), Units.degreesToRadians(-25));
+        Transform3d cam2Transform = new Transform3d(cam2Pos, cam2Rot);
+        visionSim.addCamera(cam2Sim, cam2Transform);
+        poseEstimatorCam1 = new PhotonPoseEstimator(fieldTags, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam1Transform);
+        poseEstimatorCam2 = new PhotonPoseEstimator(fieldTags, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, cam2Transform);
         previousPose = new Pose2d();
+        driveOdometry = new DifferentialDriveOdometry(new Rotation2d(), 0, 0);
+        sim_PoseEstimator = new DifferentialDrivePoseEstimator(new DifferentialDriveKinematics(DesignConstants.robotWidth), new Rotation2d(), 0, 0, new Pose2d());
     }
 
     @Override
@@ -98,6 +113,7 @@ public class DrivetrainIO_SIM implements DrivetrainIO {
         double voltsLeft = leftPid.calculate(simRobot.getLeftVelocityMetersPerSecond());
         double voltsRight = rightPid.calculate(simRobot.getRightVelocityMetersPerSecond());
         simRobot.setInputs(voltsLeft, voltsRight);
+        driveOdometry.update(new Rotation2d(), simRobot.getLeftPositionMeters(), simRobot.getRightPositionMeters());
         
         // Getting Left Side Data
         data.ampsLeft = simRobot.getLeftCurrentDrawAmps();
@@ -113,6 +129,7 @@ public class DrivetrainIO_SIM implements DrivetrainIO {
 
         data.currentPosition = simRobot.getPose();
         visionSim.update(simRobot.getPose());
+        visionSim.getDebugField().setRobotPose(simRobot.getPose());
 
         SmartDashboard.putData("Vision Sim Field", visionSim.getDebugField());
     }
@@ -125,13 +142,17 @@ public class DrivetrainIO_SIM implements DrivetrainIO {
 
     @Override
     public Pose2d getPose() {
-        poseEstimator.setReferencePose(previousPose);
-        Optional<EstimatedRobotPose> estimatedRobotPose = poseEstimator.update(cam1Sim.getCamera().getLatestResult());
-        if (estimatedRobotPose.isPresent()) {
-            previousPose = estimatedRobotPose.get().estimatedPose.toPose2d();
-            return estimatedRobotPose.get().estimatedPose.toPose2d();
-        } else {
-            return previousPose;
+        sim_PoseEstimator.update(new Rotation2d(), simRobot.getLeftPositionMeters(), simRobot.getRightPositionMeters());
+        poseEstimatorCam1.setReferencePose(driveOdometry.getPoseMeters());
+        poseEstimatorCam2.setReferencePose(driveOdometry.getPoseMeters());
+        Optional<EstimatedRobotPose> estimatedRobotPose1 = poseEstimatorCam1.update(cam1Sim.getCamera().getLatestResult());
+        Optional<EstimatedRobotPose> estimatedRobotPose2 = poseEstimatorCam2.update(cam2Sim.getCamera().getLatestResult());
+        if (estimatedRobotPose1.isPresent()) {
+            sim_PoseEstimator.addVisionMeasurement(estimatedRobotPose1.get().estimatedPose.toPose2d(), Timer.getFPGATimestamp());
         }
+        if (estimatedRobotPose2.isPresent()) {
+            sim_PoseEstimator.addVisionMeasurement(estimatedRobotPose2.get().estimatedPose.toPose2d(), Timer.getFPGATimestamp());
+        }
+        return sim_PoseEstimator.getEstimatedPosition();
     }
 }
